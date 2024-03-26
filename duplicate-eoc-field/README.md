f14304ad94aa9850da311f014935de0cc3ab0493
Dillan Asbury <dillan.asbury@juvare.com>
2024-03-25 23:26:46 -0400

    initial commit

---
+++ duplicate-eoc-field/README.md
@@ -0,0 +1,26 @@
+# duplicate-eoc-field README
+
+An extension to find duplicate eocfields in your HTML markup
+
+## Features
+
+This extension uses Javascript to scan the document and find any duplicate eocfield tags in your current view.
+
+For example if you have an eofield called 'position_name' and it is listed in your HTML like the following:
+
+![Status Bar Message](./images/Animation.gif)
+
+
+This message will appear on the right hand side of the status bar:
+
+![Status Bar Message](./images/StatusBarMessage.png)
+
+
+## Requirements
+
+This extension is very lightweight and requires no additional dependencies.
+
+
+## Release Notes
+
+Initial release; everything works as it should
---
+++ duplicate-eoc-field/extension.js
@@ -0,0 +1,60 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require('vscode');
+
+// This method is called when your extension is activated
+// Your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+function activate(context) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "duplicate-eoc-field" is now active!');
+	let activeEditor = vscode.window.activeTextEditor;
+	const statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+	vscode.workspace.onDidChangeTextDocument(activeEditor => {
+		
+		if (activeEditor) {
+			var eocfieldArray = [];
+			var duplicateLineNumbers = []
+			const lines = activeEditor.document.getText().split('\n');
+			for (let i = 0; i < lines.length; i++) {
+				const line = lines[i];
+				if(line.includes('<eocfield')){
+					var eocfieldName = line.match(/name="(.*?)"/);
+					if(eocfieldArray.includes(eocfieldName[1])){
+						duplicateLineNumbers.push(i);
+					}
+					eocfieldArray.push(eocfieldName[1]);
+					
+				}
+			}
+			const duplicates = eocfieldArray.filter((item, index) => eocfieldArray.indexOf(item) !== index);
+			if (duplicates.length == 1){
+				statusBarItem.text = `${duplicates.length} duplicate eocfield`;
+				statusBarItem.show();
+			}
+			else if(duplicates.length > 1){
+				statusBarItem.text = `${duplicates.length} duplicate eocfields`;
+				statusBarItem.show();
+			}
+			else{
+				statusBarItem.hide();
+			}
+		}
+	}, null, context.subscriptions);
+}
+
+// This method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+}
